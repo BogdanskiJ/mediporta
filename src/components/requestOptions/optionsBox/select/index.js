20073e891c3d568b7fc0286e07e8cf2e0c318ff8
Jakub <bogdanskikuba@gmail.com>
2024-04-04 00:04:24 +0200

    disable tooltip

--- src/components/requestOptions/optionsBox/select/index.js
+++ src/components/requestOptions/optionsBox/select/index.js
@@ -8,26 +8,26 @@ export const OptionsBoxSelect = ({
   $valueArray,
 }) => {
   return (
-    <Tooltip title={$title}>
-      <Box sx={{ minWidth: 120, maxWidth: 150 }}>
-        <FormControl fullWidth>
-          <InputLabel id="order-select-label">{$name}</InputLabel>
-          <Select
-            labelId="order-select-label"
-            id="order-select"
-            value={$value}
-            size="small"
-            label="Order"
-            onChange={$onChange}
-          >
-            {$valueArray.map((val) => (
-              <MenuItem key={val.name} value={val.atr}>
-                {val.name}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-      </Box>
-    </Tooltip>
+    // <Tooltip title={$title}>
+    <Box sx={{ minWidth: 120, maxWidth: 150 }}>
+      <FormControl fullWidth>
+        <InputLabel id="order-select-label">{$name}</InputLabel>
+        <Select
+          labelId="order-select-label"
+          id="order-select"
+          value={$value}
+          size="small"
+          label="Order"
+          onChange={$onChange}
+        >
+          {$valueArray.map((val) => (
+            <MenuItem key={val.name} value={val.atr}>
+              {val.name}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </Box>
+    // </Tooltip>
   );
 };
