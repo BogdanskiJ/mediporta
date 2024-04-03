90a4537216c3c95c4ddf866d60910c4362d7ef33
Jakub <bogdanskikuba@gmail.com>
2024-04-03 23:31:35 +0200

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,91 +1,39 @@
-import React, { useEffect, useState } from "react";
-import "./App.css";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchDataTags } from "./getData";
-import {
-  selectTagsBrowserData,
-  selectTagsBrowserDateFrom,
-  selectTagsBrowserDateTo,
-  selectTagsBrowserInname,
-  selectTagsBrowserMax,
-  selectTagsBrowserMin,
-  selectTagsBrowserOrder,
-  selectTagsBrowserPage,
-  selectTagsBrowserPageSize,
-  selectTagsBrowserSort,
-} from "./slice";
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectTagsBrowserError, selectTagsBrowserLoading } from "./slice";
+
 import EnhancedTable from "./components/table";
 import { RequestOptions } from "./components/requestOptions";
-import { createUrlLink } from "./components/createUrlLink";
+import { ViewBox } from "./components/viewBox";
+import { LoadingPage } from "./components/loading";
+import { ErrorPage } from "./components/error";
+
+import { StyledApp, StyledDataBox, StyledDataBoxes } from "./styled";
 
 function App() {
-  const dispatch = useDispatch();
-  const data = useSelector(selectTagsBrowserData);
-  const loading = useSelector((state) => state.loading);
-  const error = useSelector((state) => state.error);
-
-  const page = useSelector(selectTagsBrowserPage);
-  const pageSize = useSelector(selectTagsBrowserPageSize);
-  const dateFrom = useSelector(selectTagsBrowserDateFrom);
-  const dateTo = useSelector(selectTagsBrowserDateTo);
-  const order = useSelector(selectTagsBrowserOrder);
-  const min = useSelector(selectTagsBrowserMin);
-  const max = useSelector(selectTagsBrowserMax);
-  const sort = useSelector(selectTagsBrowserSort);
-  const inname = useSelector(selectTagsBrowserInname);
-
-  const [showMenu, setShowMenu] = useState(false);
-
-  // display: ${(props) => (props.$initialClicked ? "none" : "flex")};
-  // $clicked={clicked}
-  // $initialClicked={initialClicked}
-
-  const toggleMenu = () => {
-    setShowMenu(!showMenu);
-  };
-
-  if (loading) {
-    // console.log("data w loading", data);
-  }
-  if (!loading) {
-    // console.log("data w !loading", data);
-  }
-
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
+  const loading = useSelector(selectTagsBrowserLoading);
+  const error = useSelector(selectTagsBrowserError);
 
   return (
-    <div className="App">
-      <div>
-        <RequestOptions />
-      </div>
-      <div>
-        cokolwiek
-        <button onClick={toggleMenu}>pokaż menu</button>
-        <button
-          onClick={() =>
-            dispatch(
-              fetchDataTags({
-                page,
-                pageSize,
-                dateFrom,
-                dateTo,
-                order,
-                min,
-                max,
-                sort,
-                inname,
-              })
-            )
-          }
-        >
-          klik
-        </button>
-      </div>
-
-      <div>{data ? <EnhancedTable data={data} /> : ""}</div>
-    </div>
+    <StyledApp className="App">
+      <RequestOptions />
+      <StyledDataBoxes>
+        {loading ? (
+          <LoadingPage />
+        ) : error ? (
+          <ErrorPage />
+        ) : (
+          <>
+            <StyledDataBox>
+              <EnhancedTable />
+            </StyledDataBox>
+            <StyledDataBox>
+              <ViewBox />
+            </StyledDataBox>
+          </>
+        )}
+      </StyledDataBoxes>
+    </StyledApp>
   );
 }
 
