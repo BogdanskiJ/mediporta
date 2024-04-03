4d87823f542f4a6cee4b9f51d3301a1083399afb
Jakub <bogdanskikuba@gmail.com>
2024-04-03 23:31:03 +0200

    theme update

--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,37 @@
-import { createTheme } from '@mui/material/styles';
-
-export const theme = createTheme({
-    // Tu możesz dostosować motyw
-  });
+export const theme = {
+  color: {
+    gray: "#808080",
+    whisper: "#F5F5FA",
+    black: "#000000",
+    white: "#FFFFFF",
+    green: "#00FF00",
+    red: "#FF0000",
+    electricViolet: "#6827ff",
+    blue: "#0000FF",
+    concrete: "#F2F2F2",
+    gallery: "#f0f0f0",
+    denim: "#1976d2",
+    silver: "#cbcbcb",
+    alto: "#E0E0E0",
+    monarch: "#9D0C26",
+    camarone: "#006100",
+    blackOpacity004: "rgba(0, 0, 0, 0.04)",
+    blackOpacity005: "rgba(0, 0, 0, 0.05)",
+    blackOpacity007: "rgba(0, 0, 0, 0.07)",
+    blackOpacity01: "rgba(0, 0, 0, 0.1)",
+    blackOpacity01: "rgba(0, 0, 0, 0.17)",
+    blackOpacity02: "rgba(0, 0, 0, 0.2)",
+    blackOpacity01: "rgba(0, 0, 0, 0.27)",
+    blackOpacity04: "rgba(0, 0, 0, 0.4)",
+    blackOpacity05: "rgba(0, 0, 0, 0.5)",
+  },
+  breakpoint: {
+    xs: 0,
+    sm: `576px`,
+    md: `768px`,
+    lg: `992px`,
+    xl: `1200px`,
+    xxl: `1400px`,
+    max: "1920px",
+  },
+};
